8165b00b40dc83c235d29b674f3c51c2b9a885f2
Philipp Schmitt <mail@philipp-schmitt.com>
2019-03-11 14:00:48 +0100

    Update facerecog test

--- opencv-facerecognition-test.py
+++ opencv-facerecognition-test.py
@@ -3,26 +3,39 @@ import cv2
 # Create the cascade
 faceCascade = cv2.CascadeClassifier("model.xml")
 
+cv2.namedWindow("Photobooth")
+
+# Open cam stream
 cam = cv2.VideoCapture(0)
-rval, image = cam.read()
+
+if cam.isOpened(): # try to get the first frame
+    rval, image = cam.read()
+else:
+    rval = False
 
 
-# Read the image
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+while rval:
+    # Convert to grey
+    grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        grayImg,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+    )
+    # print("Found {0} faces!".format(len(faces)))
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-)
+    cv2.imshow("Photobooth", image)
 
-print("Found {0} faces!".format(len(faces)))
+    #Read new image
+    rval, frame = cam.read()
+    key = cv2.waitKey(20)
+    if key == 27:
+        # Exit on esc
+        break
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+cv2.destroyWindow("Photobooth")
