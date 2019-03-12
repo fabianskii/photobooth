724a835b1cbc7adcd8501d96e9f63da11bac8a38
Philipp Schmitt <mail@philipp-schmitt.com>
2019-03-12 09:59:10 +0100

    Fix issues provided by fabian

--- CameraStream.py
+++ CameraStream.py
@@ -1,24 +1,23 @@
-from imutils.pivideostream import PiVideoStream
-
 class CameraStream:
     def __init__(self, src=0, use_pi_camera=False, resolution=(800, 600),
-        framerate=32):
+                 framerate=32):
         if use_pi_camera:
+            from imutils.video.pivideostream import PiVideoStream
             self.stream = PiVideoStream(resolution=resolution,
-                framerate=framerate)
+                                        framerate=framerate)
 
         else:
-            from imutils import WebcamVideoStream
+            from imutils.video.webcamvideostream import WebcamVideoStream
             self.stream = WebcamVideoStream(src=src)
-            
+
     def start(self):
         return self.stream.start()
- 
+
     def update(self):
         self.stream.update()
- 
+
     def read(self):
         return self.stream.read()
- 
+
     def stop(self):
         self.stream.stop()
--- opencv-facerecognition-test.py
+++ opencv-facerecognition-test.py
@@ -1,15 +1,15 @@
 import cv2
 import copy
-from CamerStream import CameraStream
+from CameraStream import CameraStream
+
 # Create the cascade
 faceCascade = cv2.CascadeClassifier("model.xml")
 
 cv2.namedWindow("Photobooth")
-cam = CameraStream(usePiCamera=True).start()
-
-
+cam = CameraStream(use_pi_camera=True).start()
+image = cam.read()
 image_count = 0
-while true:
+while True:
     # Convert to grey
     grayImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Detect faces in the image
@@ -23,16 +23,16 @@ while true:
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    
+
     cv2.imshow("Photobooth", image)
     if (len(faces)) > 0:
         # TODO generate image folder if not exists
-        print ('image_{0}'.format(image_count))
-        cv2.imwrite('images/image_{0}.jpg'.format(image_count),source_image)
+        print('image_{0}'.format(image_count))
+        cv2.imwrite('images/image_{0}.jpg'.format(image_count), source_image)
         image_count += 1
-        
-    #Read new image
-    rval, image = cam.read()
+
+    # Read new image
+    image = cam.read()
     key = cv2.waitKey(20)
     if key == 27:
         # Exit on esc
